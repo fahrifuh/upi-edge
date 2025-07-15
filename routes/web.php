6337ac09298f7b29c279a722c023b75ca871f082
fahrifuh <muhammadfahri571@gmail.com>
2025-07-15 08:14:42 +0700

    add route for lecturers

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LecturerController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,7 +17,15 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    // Route::resource('lecturers');
+    Route::resource('lecturers', LecturerController::class)->names([
+        'index' => 'lecturer.index',
+        'create' => 'lecturer.create',
+        'store' => 'lecturer.store',
+        'show' => 'lecturer.show',
+        'edit' => 'lecturer.edit',
+        'update' => 'lecturer.update',
+        'destroy' => 'lecturer.destroy',
+    ]);
 });
 
 require __DIR__ . '/auth.php';
