12ae14896a0d5edfa0751b481abf9e7586c466ff
fahrifuh <muhammadfahri571@gmail.com>
2025-07-16 13:33:04 +0700

    create CRUD Lecturer (Controller and Pages)

--- app/Http/Controllers/LecturerController.php
+++ app/Http/Controllers/LecturerController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Lecturer;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LecturerController extends Controller
 {
@@ -29,7 +30,42 @@ class LecturerController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:lecturers,email',
+            'phone' => 'required|string|max:15',
+            'address' => 'required|string',
+            'gender' => 'required|in:l,p',
+            'birth_date' => 'required|date',
+        ], [
+            'name.required' => 'Nama harus diisi.',
+            'email.required' => 'Email harus diisi.',
+            'email.unique' => 'Email sudah terdaftar.',
+            'phone.required' => 'Nomor telepon harus diisi.',
+            'address.required' => 'Alamat harus diisi.',
+            'gender.required' => 'Jenis kelamin harus dipilih.',
+            'birth_date.required' => 'Tanggal lahir harus diisi.',
+            'birth_date.date' => 'Tanggal lahir harus berupa tanggal yang valid.',
+        ]);
+
+        $data = [
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'address' => $request->address,
+            'gender' => $request->gender,
+            'birth_date' => $request->birth_date,
+        ];
+
+        $post = Lecturer::create($data);
+
+        activity()
+            ->performedOn($post)
+            ->event('create')
+            ->causedBy(Auth::user())
+            ->log('Dosen baru ditambahkan: ' . $request->name);
+
+        return redirect()->route('lecturer.index')->with('success', 'Berhasil tambah data dosen.');
     }
 
     /**
@@ -54,7 +90,55 @@ class LecturerController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:lecturers,email,' . $id,
+            'phone' => 'required|string|max:15',
+            'address' => 'required|string',
+            'gender' => 'required|in:l,p',
+            'birth_date' => 'required|date',
+        ], [
+            'name.required' => 'Nama harus diisi.',
+            'email.required' => 'Email harus diisi.',
+            'email.unique' => 'Email sudah terdaftar.',
+            'phone.required' => 'Nomor telepon harus diisi.',
+            'address.required' => 'Alamat harus diisi.',
+            'gender.required' => 'Jenis kelamin harus dipilih.',
+            'birth_date.required' => 'Tanggal lahir harus diisi.',
+            'birth_date.date' => 'Tanggal lahir harus berupa tanggal yang valid.',
+        ]);
+
+        $data = [
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'address' => $request->address,
+            'gender' => $request->gender,
+            'birth_date' => $request->birth_date,
+        ];
+
+        $lecturer = Lecturer::findOrFail($id);
+        $beforeUpdate = $lecturer->getOriginal();
+        $lecturer->update($data);
+
+        $changes = [];
+        foreach ($data as $key => $value) {
+            if (array_key_exists($key, $beforeUpdate) &&  $beforeUpdate[$key] !== $value) {
+                $changes[$key] = [
+                    'old' => $beforeUpdate[$key],
+                    'new' => $value,
+                ];
+            }
+        }
+
+        activity()
+            ->performedOn($lecturer)
+            ->event('update')
+            ->withProperties(['changes' => $changes])
+            ->causedBy(Auth::user())
+            ->log('Dosen dengan ID ' . $lecturer->id . ' berhasil diupdate');
+
+        return redirect()->route('lecturer.index')->with('success', 'Berhasil update data dosen.');
     }
 
     /**
@@ -62,6 +146,15 @@ class LecturerController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $lecturer = Lecturer::findOrFail($id);
+        $lecturer->delete();
+
+        activity()
+            ->performedOn($lecturer)
+            ->event('delete')
+            ->causedBy(Auth::user())
+            ->log('Dosen dihapus: ' . $lecturer->name);
+
+        return redirect()->route('lecturer.index')->with('success', 'Berhasil hapus data dosen.');
     }
 }
