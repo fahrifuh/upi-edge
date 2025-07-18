20be0125832cfdf2cc22bed970119e3828bbfa53
fahrifuh <muhammadfahri571@gmail.com>
2025-07-18 08:56:51 +0700

    refactor: update header text

--- resources/views/pages/activity-schedule/index.blade.php
+++ resources/views/pages/activity-schedule/index.blade.php
@@ -44,7 +44,7 @@
     @endpush
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Data Mahasiswa') }}
+            {{ __('Jadwal Kegiatan') }}
         </h2>
     </x-slot>
 
