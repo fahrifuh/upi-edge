3c25c1807bbf601af652d60a0cf0ca4019ad16ce
fahrifuh <muhammadfahri571@gmail.com>
2025-07-22 13:52:03 +0700

    feat: create card summary component

---
+++ resources/views/components/card-summary.blade.php
@@ -0,0 +1,9 @@
+<div class="bg-white flex justify-between items-center p-4 rounded-lg h-100">
+    <div class="flex flex-col gap-4 h-100">
+        <h3 class="text-sm font-medium">{{ $title }}</h3>
+        <p class="text-sm font-semibold">{{ $total }}</p>
+    </div>
+    <div class="bg-primary text-white rounded-full flex items-center justify-center p-3">
+        <i class="fa-solid {{ $icon }}"></i>
+    </div>
+</div>
