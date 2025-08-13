b7e59a1befba4b7b15bc67538c6ecac21161efe5
fahrifuh <muhammadfahri571@gmail.com>
2025-08-13 14:39:13 +0700

    feat: create new scope to get data just for current user

--- app/Models/ActivitySchedule.php
+++ app/Models/ActivitySchedule.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class ActivitySchedule extends Model
 {
@@ -19,5 +20,24 @@ class ActivitySchedule extends Model
     public function students()
     {
         return $this->belongsToMany(Student::class, 'activity_schedules_students');
+    }
+
+    public function scopeForCurrentUser($query)
+    {
+        $user = Auth::user();
+
+        if($user->role == 'mahasiswa' && $user->student){
+            return $query->whereHas('students', function($q) use ($user){
+                $q->where('student_id', $user->student->id);
+            });
+        }
+        
+        if($user->role == 'dosen' && $user->lecturer){
+            return $query->whereHas('lecturers', function($q) use ($user){
+                $q->where('lecturer_id', $user->lecturer->id);
+            });
+        }
+
+        return $query;
     }
 }
