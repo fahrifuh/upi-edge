e2e2c07485b3985d8ffc47dc63d12efd2dc6e3d3
fahrifuh <muhammadfahri571@gmail.com>
2025-08-19 10:57:55 +0700

    feat: add route for gemini response

--- routes/api.php
+++ routes/api.php
@@ -11,3 +11,4 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/rsc-data', [RSCDataController::class, 'handleSensorData']);
 Route::get('/application-settings', [ApplicationSettingController::class, 'fetchApplicationSettings']);
+Route::get('/rekomendasi-tanaman', [RSCDataController::class, 'getRekomendasiTanaman']);
