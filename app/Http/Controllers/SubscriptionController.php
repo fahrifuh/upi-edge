4957615d9e927c45bda9528c804c8a2f2d7c627e
fahrifuh <muhammadfahri571@gmail.com>
2025-08-25 11:34:20 +0700

    feat: create controller for subscription, create function for CRUD subscription plan

---
+++ app/Http/Controllers/SubscriptionController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SubscriptionPlan;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class SubscriptionController extends Controller
+{
+    public function index()
+    {
+        return view('pages.subscription.index');
+    }
+
+    public function indexPlan()
+    {
+        $plans = SubscriptionPlan::get();
+        return view('pages.subscription.plan.index', compact('plans'));
+    }
+
+    public function indexHistory()
+    {
+        return view('pages.subscription.history.index');
+    }
+
+    public function createPlan()
+    {
+        return view('pages.subscription.plan.create');
+    }
+
+    public function storePlan(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'quota' => 'nullable|integer',
+            'price' => 'nullable|integer'
+        ], [
+            'name.required' => 'Nama paket harus diisi.',
+            'name.string' => 'Nama paket harus berupa string yang valid.',
+            'quota.integer' => 'Quota harus berupa angka yang valid.',
+            'price.integer' => 'Harga harus berupa angka yang valid.',
+        ]);
+
+        $data = [
+            'name' => $request->name,
+            'quota' => $request->quota,
+            'price' => $request->price
+        ];
+
+        $post = SubscriptionPlan::create($data);
+
+        activity()
+            ->performedOn($post)
+            ->event('create')
+            ->causedBy(Auth::user())
+            ->log('Paket langganan baru ditambahkan: ' . $request->name);
+
+        return redirect()->route('subscription.plan.index')->with('success', 'Data Paket Langganan berhasil ditambahkan.');
+    }
+
+    public function editPlan($id)
+    {
+        $plan = SubscriptionPlan::findOrFail($id);
+        return view('pages.subscription.plan.edit', compact('plan'));
+    }
+
+    public function updatePlan(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'quota' => 'nullable|integer',
+            'price' => 'nullable|integer'
+        ], [
+            'name.required' => 'Nama paket harus diisi.',
+            'name.string' => 'Nama paket harus berupa string yang valid.',
+            'quota.integer' => 'Quota harus berupa angka yang valid.',
+            'price.integer' => 'Harga harus berupa angka yang valid.',
+        ]);
+
+        $data = [
+            'name' => $request->name,
+            'quota' => $request->quota,
+            'price' => $request->price
+        ];
+
+        $plan = SubscriptionPlan::findOrFail($id);
+        $beforeUpdate = $plan->getOriginal();
+        $plan->update($data);
+
+        $changes = [];
+        foreach ($data as $key => $value) {
+            if (array_key_exists($key, $beforeUpdate) &&  $beforeUpdate[$key] !== $value) {
+                $changes[$key] = [
+                    'old' => $beforeUpdate[$key],
+                    'new' => $value,
+                ];
+            }
+        }
+
+        activity()
+            ->performedOn($plan)
+            ->event('update')
+            ->withProperties(['changes' => $changes])
+            ->causedBy(Auth::user())
+            ->log('Paket langganan ' . $plan->name . ' berhasil diupdate');
+
+        return redirect()->route('subscription.plan.index')->with('success', 'Data Paket Langganan berhasil diupdate.');
+    }
+}
